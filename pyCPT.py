fc169a883cca300d88ffcdd879092c02a160b2fb
hwang <wang@aices.rwth-aachen.de>
2018-05-21 20:18:26 +0200

    reconstructed the pyCPT file

--- pyCPT.py
+++ pyCPT.py
@@ -3,90 +3,129 @@ import pyHMRF
 import matplotlib.pyplot as plt
 from scipy.stats import multivariate_normal
 from itertools import groupby
+from pre_process import model_selection
 
 
-def read_cpt_data(datapath):
+class CPT:
+    def __init__(self, data_path, thin_layer_criteria=0.25, max_number_of_clusters=10, plot_mod_sel=True):
+        """
+
+        :param
+        data_path: path to the dataset
+        max_number_of_clusters: maximum possible number of clusters
+        """
+
+        # set the thin layer criteria
+        self.thinLayerCriteria = thin_layer_criteria
+        # initial data structure
+        self.element = read_cpt_data(data_path)
+        print('Number of data points:  ' + str(self.element.phys_shp))
+        # perform model selection
+        self.mod_sel = model_selection(self.element.feat, max_number_of_clusters, plot=plot_mod_sel)
+        # initialize layer_info with nan
+        self.layer_info = np.nan
+
+    def segmentation(self, num_of_iter, start_iter, beta_init=1, beta_jump_length=15):
+
+        """
+        :param
+        :param num_of_iter: the number of iterations
+        :param start_iter: the starting iter_ID of the converged Markov chain
+        :param beta_init: initial value of beta
+        :param beta_jump_length: the jump length of beta during MCMC sampling
+        :return:
+        """
+
+        self.element.fit(n=num_of_iter, n_labels=self.mod_sel[1], beta_init=beta_init,
+                         beta_jump_length=beta_jump_length)
+        self.element.get_estimator(start_iter=start_iter)
+        self.element.get_label_prob(start_iter=start_iter)
+        self.element.get_map()
+        self.element.get_ie()
+
+    def detect_layers(self):
+        """
+
+        :return:
+            hardBoundary: location of hard layer boundaries
+            softBoundary: location of soft layer boundaries
+            n_layers: the number of layers
+            prob_SBT: probability of each layer belonging to each SBT
+            soil_SBT_type: SBT index of each layer
+            label_of_layers: label of each layer
+
+        """
+
+        if type(self.element.label_map_est) is float:
+            if np.isnan(self.element.label_map_est):
+                print('Segmentation has not been performed!')
+                return
+
+        temp = np.concatenate(([0], np.diff(self.element.label_map_est)), axis=0)
+        bnd_idx = temp != 0
+        boundary_points = self.element.coords[bnd_idx] - 0.5 * (self.element.coords[2] - self.element.coords[1])
+        thickness = np.diff(np.concatenate(([0], boundary_points.ravel(), self.element.coords[-1])))
+        thin_idx = np.argwhere(thickness <= self.thinLayerCriteria)
+
+        if len(thin_idx) > 0:
+            for i in range(len(thin_idx)):
+                if thin_idx[i] == 0:
+                    boundary_points[0] = np.nan
+                else:
+                    if thin_idx[i] == len(boundary_points) + 1:
+                        boundary_points[-1] = np.nan
+                    else:
+                        boundary_points[thin_idx[i] - 1] = 0.5 * (boundary_points[thin_idx[i] - 1] +
+                                                                  boundary_points[thin_idx[i]])
+                        boundary_points[thin_idx[i]] = np.nan
+
+            boundary_points = np.array([boundary_points[~np.isnan(boundary_points)]]).T
+
+        n_layers = len(boundary_points) + 1
+
+        flag = np.concatenate(([0], boundary_points.ravel(), self.element.coords[-1] + 0.01))
+        prob_sbt = np.zeros((9, n_layers))
+        soil_sbt_type = np.zeros((n_layers,))
+        label_of_layers = np.zeros((n_layers,))
+
+        for i in range(n_layers):
+            find_idx = (self.element.coords.ravel() > flag[i]) & (self.element.coords.ravel() < flag[i + 1])
+            prob_sbt[:, i] = two_d_label(self.element.feat[find_idx, 0], self.element.feat[find_idx, 1])
+            soil_sbt_type[i] = np.argmax(prob_sbt[:, i]) + 1
+            temp = self.element.label_map_est[find_idx]
+            unique, counts = np.unique(temp, return_counts=True)
+            label_of_layers[i] = unique[np.argmax(counts)]
+
+        hard_boundary = []
+        soft_boundary = []
+
+        for i in range(len(boundary_points)):
+            if soil_sbt_type[i] != soil_sbt_type[i + 1]:
+                hard_boundary.append(boundary_points[i])
+            elif label_of_layers[i] != label_of_layers[i + 1]:
+                soft_boundary.append(boundary_points[i])
+
+        self.layer_info = [hard_boundary, soft_boundary, n_layers, prob_sbt, soil_sbt_type, label_of_layers]
+
+
+def read_cpt_data(data_path):
     """    
     :param 
-        datapath (str): path to the data file 
+        data_path (str): path to the data file
     :return: 
         
     """
-    cpt_data = np.genfromtxt(datapath, delimiter=',')
+    cpt_data = np.genfromtxt(data_path, delimiter=',')
     coord = np.array([np.nanmin(cpt_data[:, 0]), np.nanmax(cpt_data[:, 0])])
     data = np.log10(cpt_data[:, 1:])
-    cpt = pyHMRF.Element(data, coord, normalize=False)
-    return cpt
+    element = pyHMRF.Element(data, coord, normalize=False)
+    return element
 
 
-def detect_layers(cpt, thinLayerCriteria=0.25):
-    """
-
-    :param
-        cpt (:obj:`pyHMRF.Element`): the data structure for cpt data
-        thinLayerCriteria (:obj: 'float'): thin layer crrteria
-
-    :return:
-        hardBoundary: location of hard layer boundaries
-        softBoundary: location of soft layer boundaries
-        n_layers: the number of layers
-        prob_SBT: probability of each layer belonging to each SBT
-        soil_SBT_type: SBT index of each layer
-        label_of_layers: label of each layer
-
-    """
-
-    temp = np.concatenate(([0], np.diff(cpt.label_map_est)), axis=0)
-    bnd_idx = temp != 0
-    bndp = cpt.coords[bnd_idx] - 0.5 * (cpt.coords[2] - cpt.coords[1])
-    thickness = np.diff(np.concatenate(([0], bndp.ravel(), cpt.coords[-1])))
-    thin_idx = np.argwhere(thickness <= thinLayerCriteria)
-
-    if len(thin_idx) > 0:
-        for i in range(len(thin_idx)):
-            if thin_idx[i] == 0:
-                bndp[0] = np.nan
-            else:
-                if thin_idx[i] == len(bndp) + 1:
-                    bndp[-1] = np.nan
-                else:
-                    bndp[thin_idx[i] - 1] = 0.5 * (bndp[thin_idx[i] - 1] + bndp[thin_idx[i]])
-                    bndp[thin_idx[i]] = np.nan
-
-        bndp = np.array([bndp[~np.isnan(bndp)]]).T
-
-    n_layers = len(bndp) + 1
-
-    flag = np.concatenate(([0], bndp.ravel(), cpt.coords[-1] + 0.01))
-    prob_SBT = np.zeros((9, n_layers))
-    soil_SBT_type = np.zeros((n_layers,))
-    label_of_layers = np.zeros((n_layers,))
-
-    for i in range(n_layers):
-        find_idx = (cpt.coords.ravel() > flag[i]) & (cpt.coords.ravel() < flag[i + 1])
-        prob_SBT[:, i] = TwoD_label(cpt.feat[find_idx, 0], cpt.feat[find_idx, 1])
-        soil_SBT_type[i] = np.argmax(prob_SBT[:, i]) + 1
-        temp = cpt.label_map_est[find_idx]
-        unique, counts = np.unique(temp, return_counts=True)
-        label_of_layers[i] = unique[np.argmax(counts)]
-
-    hardBoundary = []
-    softBoundary = []
-
-    for i in range(len(bndp)):
-        if soil_SBT_type[i] != soil_SBT_type[i + 1]:
-            hardBoundary.append(bndp[i])
-        elif label_of_layers[i] != label_of_layers[i + 1]:
-            softBoundary.append(bndp[i])
-
-    layer_info = [hardBoundary, softBoundary, n_layers, prob_SBT, soil_SBT_type, label_of_layers]
-    return layer_info
-
-
-def TwoD_label(log_Fr, log_Qt):
-    temp = np.zeros([log_Fr.shape[0], 2])
-    temp[:, 0] = np.log(10**log_Fr)
-    temp[:, 1] = np.log(10**log_Qt)
+def two_d_label(log_fr, log_qt):
+    temp = np.zeros([log_fr.shape[0], 2])
+    temp[:, 0] = np.log(10 ** log_fr)
+    temp[:, 1] = np.log(10 ** log_qt)
     c = soil_label(temp)
     x = np.zeros([9, ])
     for i in range(x.shape[0]):
@@ -132,9 +171,9 @@ def soil_label(x):
     return labeled_x
 
 
-def mixture_plot_robertsonchart(cpt, res=50, figsize=(8, 4.8),
-                                cmap_contour='viridis', cmap_scatter='Pastel2',
-                                lw=1, equal_axis=False, aspect=0.8):
+def mixture_plot_robertson_chart(cpt, res=50, figsize=(8, 4.8),
+                                 cmap_contour='viridis', cmap_scatter='Pastel2',
+                                 lw=1, equal_axis=False, aspect=0.8):
     """
     purpose: re-construct the SBT zones based on Robertson (1990)
         The boundaries of SBT zones refer to Wang et al. (2013).
@@ -149,7 +188,7 @@ def mixture_plot_robertsonchart(cpt, res=50, figsize=(8, 4.8),
     pos[:, :, 0] = X
     pos[:, :, 1] = Y
 
-    n_label = cpt.mu_est.shape[0]
+    n_label = cpt.element.mu_est.shape[0]
 
     if equal_axis:
         plt.axes().set_aspect('equal')
@@ -157,14 +196,14 @@ def mixture_plot_robertsonchart(cpt, res=50, figsize=(8, 4.8),
         plt.axes().set_aspect(aspect=aspect)
 
     for i in range(n_label):
-        mu_temp = cpt.mu_est[i, :]
-        cov_temp = cpt.cov_est[i, :, :]
+        mu_temp = cpt.element.mu_est[i, :]
+        cov_temp = cpt.element.cov_est[i, :, :]
         Z = multivariate_normal(mu_temp, cov_temp).pdf(pos)
         plt.contour(X, Y, Z, cmap=cmap_contour, linewidths=lw)
 
-    plt.scatter(cpt.feat[:, 0], cpt.feat[:, 1], s=5, c=cpt.label_map_est, cmap=cmap_scatter)
+    plt.scatter(cpt.element.feat[:, 0], cpt.element.feat[:, 1], s=5, c=cpt.element.label_map_est, cmap=cmap_scatter)
 
-    # plot background Robertsonchart
+    # plot background Robertson chart
     Fr = np.zeros([64, 8])
     Qt = np.zeros([64, 8])
     Fr[:, 0] = np.linspace(-2.3026, 0.6569, 64)
@@ -198,7 +237,7 @@ def mixture_plot_robertsonchart(cpt, res=50, figsize=(8, 4.8),
         plt.plot(Fr[:, i], Qt[:, i], color='black', linewidth=1, zorder=-3)
 
     # setting axes parameters
-    plt.title('Statistical pattern on Robertsonchart', fontsize=16)
+    plt.title('Statistical pattern on Robertson chart', fontsize=16)
     plt.xlabel(r'log$F_r$', fontsize=14)
     plt.ylabel(r'log$Q_t$', fontsize=14)
     plt.axis([-1, 1, 0, 3])
@@ -210,7 +249,7 @@ def mixture_plot_robertsonchart(cpt, res=50, figsize=(8, 4.8),
     plt.show()
 
 
-def plot_layers(cpt, layer_info, figsize=(12, 8), aspect=0.1):
+def plot_layers(cpt, figsize=(12, 8), aspect=0.1):
     """
     :param:
     cpt:
@@ -223,7 +262,7 @@ def plot_layers(cpt, layer_info, figsize=(12, 8), aspect=0.1):
     # plot information entropy
     plt.figure(figsize=figsize)
     plt.axes().set_aspect(aspect=aspect)
-    plt.plot(cpt.info_entr, cpt.coords, 'red', linewidth=1)  # rotate x axis to y axis,now y corresponds to depth
+    plt.plot(cpt.element.info_entr, cpt.element.coords, 'red', linewidth=1)
     plt.gca().invert_yaxis()  # y is increasing along depth
     plt.xlabel('Information entropy', fontsize=14)
     plt.ylabel('Depth (m)', fontsize=14)
@@ -233,49 +272,30 @@ def plot_layers(cpt, layer_info, figsize=(12, 8), aspect=0.1):
     plt.xlim(0, 1.25)
 
     # plot boundaries
-    hardboundary = np.tile(layer_info[0], 2)
-    softboundary = np.tile(layer_info[1], 2)
-    soil_SBT = layer_info[4]
-    soil_SBT_unique = [x[0] for x in groupby(soil_SBT)]
-    xaxis = np.array([0, 1.25])
-    temp_1 = np.concatenate((cpt.coords[0], np.array(layer_info[0]).ravel()))
-    temp_2 = np.concatenate((np.array(layer_info[0]).ravel(), cpt.coords[-1]))
+    hard_boundary = np.tile(cpt.layer_info[0], 2)
+    soft_boundary = np.tile(cpt.layer_info[1], 2)
+    soil_sbt = cpt.layer_info[4]
+    soil_sbt_unique = [x[0] for x in groupby(soil_sbt)]
+    x_axis = np.array([0, 1.25])
+    temp_1 = np.concatenate((cpt.element.coords[0], np.array(cpt.layer_info[0]).ravel()))
+    temp_2 = np.concatenate((np.array(cpt.layer_info[0]).ravel(), cpt.element.coords[-1]))
     text_location_y = (temp_1+temp_2)/2
     text_location_x = 1
 
-    if len(hardboundary) > 0:
-        for i in range(np.shape(hardboundary)[0]):
-            plt.plot(xaxis, hardboundary[i, :], 'black', linestyle='-', linewidth=2)
+    if len(hard_boundary) > 0:
+        for i in range(np.shape(hard_boundary)[0]):
+            plt.plot(x_axis, hard_boundary[i, :], 'black', linestyle='-', linewidth=2)
     else:
         pass
 
-    if len(softboundary) > 0:
-        for i in range(np.shape(softboundary)[0]):
-            plt.plot(xaxis, softboundary[i, :], 'grey', linestyle='--', linewidth=1)
+    if len(soft_boundary) > 0:
+        for i in range(np.shape(soft_boundary)[0]):
+            plt.plot(x_axis, soft_boundary[i, :], 'grey', linestyle='--', linewidth=1)
     else:
         pass
 
     for i, value in enumerate(text_location_y):
-        plt.text(text_location_x, value, 'SBT* = '+str(int(soil_SBT_unique[i])), fontsize=14)
+        plt.text(text_location_x, value, 'SBT* = '+str(int(soil_sbt_unique[i])), fontsize=14)
 
     plt.grid(False)
     plt.show()
-
-
-def segmentation(cpt, n, n_labels, start_iter, beta_init=1, beta_jump_length=15):
-    """
-
-    :param cpt: the object from pyHMRF.Element()
-    :param n: the number of iterations
-    :param n_labels: the number of labels
-    :param start_iter: the starting iter_ID of the converged Markov chain
-    :param beta_init: initial value of beta
-    :param beta_jump_length: the jump length of beta during MCMC sampling
-    :return:
-    """
-
-    cpt.fit(n, n_labels, beta_init, beta_jump_length)
-    cpt.get_estimator(start_iter=start_iter)
-    cpt.get_label_prob(start_iter=start_iter)
-    cpt.get_map()
-    cpt.get_ie()
